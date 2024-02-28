b19b5ad1cf0bf5f9795c48c53e3e115e9e9fdb63
권현준 <bonheur9813@naver.com>
2024-02-28 20:33:23 +0900

    ✨: 회원관련 DB 파일 생성
     UserEntity & UserRepository

---
+++ SpringJWT/src/main/java/org/example/springjwt/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.example.springjwt.repository;
+
+import org.example.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
